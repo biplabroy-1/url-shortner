b2561fb064fd26f253ab8ff55ce7c7ed77f2d825
Biplab Roy <149038053+biplabroy-1@users.noreply.github.com>
2025-04-17 19:31:08 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/", "/api/stats", "/api/shorten", "/u(.*)"],
   clockSkewInMs: 30000,
+  authorizedParties: ["https://url.globaltfn.tech"]
 });
 
 export const config = {
